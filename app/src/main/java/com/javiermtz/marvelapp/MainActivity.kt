ab0853fdc8a41c4dfeb1bf156840f3c355cb70c3
Javier Martinez <javierrrr.15@gmail.com>
2022-01-29 02:16:14 -0600

    Set project and Git

---
+++ app/src/main/java/com/javiermtz/marvelapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.javiermtz.marvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activit y_main)
+  }
+}
